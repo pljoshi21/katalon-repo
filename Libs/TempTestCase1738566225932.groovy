d48dedf930f6321f6433dd019098d74b65fdd7cc
poojajoshi01 <pooja.joshi@josys.com>
2025-03-10 15:08:26 +0530

    unwanted changes

---
+++ Libs/TempTestCase1738566225932.groovy
@@ -0,0 +1,26 @@
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
+import com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner
+import com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner
+import com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner
+
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile('/tmp/Katalon/20250203_123345/execution.properties')
+
+TestCaseMain.beforeStart()
+
+        TestCaseMain.runWSVerificationScript(new TestCaseBinding('',[:]), 'import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI\nimport com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile\nimport com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW\nimport com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS\nimport com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows\nimport static com.kms.katalon.core.testobject.ObjectRepository.findTestObject\nimport static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject\nimport static com.kms.katalon.core.testdata.TestDataFactory.findTestData\nimport static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase\nimport static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint\nimport com.kms.katalon.core.model.FailureHandling as FailureHandling\nimport com.kms.katalon.core.testcase.TestCase as TestCase\nimport com.kms.katalon.core.testdata.TestData as TestData\nimport com.kms.katalon.core.testobject.TestObject as TestObject\nimport com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint\nimport internal.GlobalVariable as GlobalVariable\nimport org.openqa.selenium.Keys as Keys\n\nWebUI.callTestCase(findTestCase(\'LoginSuccessful\'), [:], FailureHandling.STOP_ON_FAILURE)\n\nWebUI.selectOptionByValue(findTestObject(\'Object Repository/Page_CURA Healthcare Service/select_Tokyo CURA Healthcare Center        _5b4107\'), \n    \'Hongkong CURA Healthcare Center\', true)\n\nWebUI.click(findTestObject(\'Object Repository/Page_CURA Healthcare Service/label_Apply for hospital readmission\'))\n\nWebUI.click(findTestObject(\'Object Repository/Page_CURA Healthcare Service/div_Healthcare Program                     _4552b2\'))\n\nWebUI.click(findTestObject(\'Object Repository/Page_CURA Healthcare Service/label_Medicaid\'))\n\nWebUI.click(findTestObject(\'Object Repository/Page_CURA Healthcare Service/input_Visit Date (Required)_visit_date\'))\n\nWebUI.click(findTestObject(\'Object Repository/Page_CURA Healthcare Service/td_25\'))\n\nWebUI.setText(findTestObject(\'Object Repository/Page_CURA Healthcare Service/textarea_Comment_comment\'), \'test\')\n\nWebUI.click(findTestObject(\'Object Repository/Page_CURA Healthcare Service/button_Book Appointment\'))\n\nWebUI.navigateToUrl(\'https://katalon-demo-cura.herokuapp.com/appointment.php#summary\')\n\n', FailureHandling.STOP_ON_FAILURE, true)
+
